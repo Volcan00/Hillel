68240552acbfabb0270a3adc8a8364988cbe4e94
Volcan00 <45773869+Volcan00@users.noreply.github.com>
2023-03-11 14:13:46 +0100

    homework lesson 16

--- add12.py
+++ add12.py
@@ -5,36 +5,42 @@ import al_db
 import models_db
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from flask import session as flask_session
+from flask_session import Session as FlaskSession
 
 app = Flask(__name__)
+app.secret_key = "supersecret"
 
 @app.route("/", methods = ['GET', 'POST'])
 def login_user():
-    if request.method == 'GET':
-        record_1 = models_db.Currency(bank = "GGG", currency = "USD", date_exchange = "2023-02-19", buy_rate = 36.6, sale_rate = 37.45)
+    if request.method == 'POST':
         with Session(al_db.engine) as session:
-            session.add(record_1)
-            session.commit()
-            return "OK"
+            query = select(models_db.User).filter(models_db.User.username == request.form["usernamelogin"])
+            result = session.execute(query).fetchall()
+            if result:
+                flask_session["username"] = request.form["username"]
+            else:
+                return render_template("index.html", username = "No user found")
+            return render_template("index.html", username = flask_session["username"])
     else: 
-        pass
-    return "<p>Login!</p>"
+        return render_template("login.html")
 
 @app.route("/logout", methods = ['GET'])
 def logout_user():
-    if request.method == 'GET':
-        return "OK"
+    flask_session.pop("username", None)
     return "<p>Logout</p>"
+
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register_user():
+        return "<p>Logout</p>"
     
 
 @app.route("/user_page", methods = ['GET'])
-def user_access():
-    if request.method == 'POST':
-        return "OK"
-    return "More functions"
+def index():
+    if "username" in flask_session:
+        return f'Logged in as {flask_session["username"]}'
+    return "You are not logged in"
 
 @app.route("/currency", methods = ['GET', 'POST'])
 def currency_convert():
@@ -56,9 +62,17 @@ def currency_convert():
         cur_exchange_buy = buy_rate_2 / buy_rate_1
         cur_exchange_sale = sale_rate_2 / sale_rate_1
 
-        return render_template('data_form.html', cur_exchange_buy = cur_exchange_buy, cur_exchange_sale = cur_exchange_sale, user_currency_1 = user_currency_1, user_currency_2 = user_currency_2)
+        return render_template('data_form.html', 
+                               cur_exchange_buy = cur_exchange_buy, 
+                               cur_exchange_sale = cur_exchange_sale, 
+                               user_currency_1 = user_currency_1, 
+                               user_currency_2 = user_currency_2,
+                               username = flask_session["username"])
     else:
-        return render_template('data_form.html')
-
+        if "username" in flask_session:
+            return render_template('data_form.html', username = flask_session["username"])
+        else:
+            return "LOGIN FIRST"
+        
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
